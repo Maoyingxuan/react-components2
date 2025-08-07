c926bc81bad17bef6395467da7f288d5de91605e
Maoyingxuan <yinmao1@student.unimelb.edu.au>
2025-08-07 16:59:08 +1000

    reconciler实现准备

--- mini-react/examples/src/main.tsx
+++ mini-react/examples/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {ReactDOM} from "../whichreact"
+const jsx = (
+  <div className="border">
+    {/* <h1>react</h1>
+    <a>mini react</a> */}
+    {/* <FunctionComponent name="函数组件" /> */}
+    {/* <ClassComponent name="类组件" /> */}
+    {/* 111
+    <ul>
+      <>
+      <li>1</li>
+      <li>2</li>
+      </>
+    </ul> */}
+    {/* <FragmentComponent /> */}
+    123
+  </div>
+);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(jsx);
+// createRoot(document.getElementById('root')!).render()
